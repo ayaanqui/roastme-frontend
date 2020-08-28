54a5413fc0c354b001480be6d46cac05ac1e6522
ayaanqui <moahammedayaan.dev@gmail.com>
2020-08-27 23:23:02 -0500

    Cleaned code

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
