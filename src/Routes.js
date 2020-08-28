dfac379d3c063400fcdd34a353b10ce1e6df62dc
ayaanqui <moahammedayaan.dev@gmail.com>
2020-08-28 11:31:50 -0500

    Added register route

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,16 @@ import {
 } from "react-router-dom";
 import Login from './Pages/Login';
 import Home from './Pages/Home';
+import Register from './Pages/Register';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path='/login'>
         <Login />
+      </Route>
+      <Route path='/register'>
+        <Register />
       </Route>
       <Route path=''>
         <Home />
