c4c19747d5706d34d0839365bdca10a76582309d
ayaanqui <moahammedayaan.dev@gmail.com>
2020-08-28 20:44:13 -0500

    Changed App to class-based component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-// import Button from 'react-bootstrap/Button';
 import { Container } from 'react-bootstrap';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <BrowserRouter>
+class App extends React.Component {
+  render = () => {
+    return (
+      <BrowserRouter>
+        <Navbar />
 
-      <Navbar />
-
-      <Container className="mt-4">
-        <Routes />
-      </Container>
-    </BrowserRouter>
-  );
+        <Container className="mt-4">
+          <Routes />
+        </Container>
+      </BrowserRouter>
+    );
+  }
 };
 
 export default App;
