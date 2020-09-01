7e02e8d9644b77a959d2a6fcbdb5c8eeb992e9b6
ayaanqui <moahammedayaan.dev@gmail.com>
2020-08-31 23:32:17 -0500

    tokenAction default export

--- src/actions/tokenAction.js
+++ src/actions/tokenAction.js
@@ -1,8 +1,8 @@
-const { default: tokenReducer } = require("../reducers/token");
-
-export const tokenAction = token => {
+const tokenAction = token => {
   return {
     type: 'LOGIN',
     payload: token,
   };
 };
+
+export default tokenAction;
