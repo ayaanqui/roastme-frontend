1b9465abc350b6da600056b1de9c7eac2e99ed8b
ayaanqui <moahammedayaan.dev@gmail.com>
2020-09-02 15:41:15 -0500

    Changed register endpoint

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ const url = 'http://localhost:3001';
 export default {
   base: url,
   login: `${url}/auth/login`,
-  register: `${url}/auth/login`,
+  register: `${url}/auth/register`,
   roasts: `${url}/roasts/`,
 };
