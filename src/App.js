48621f59bb76f95a681116e9fece1fa3b1701dab
ayaanqui <moahammedayaan.dev@gmail.com>
2020-09-02 21:10:21 -0500

    Update global state from localStorage if token is valid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Container } from 'react-bootstrap';
 import Navbar from './components/Navbar';
 import Routes from './Routes';
 import { BrowserRouter } from 'react-router-dom';
+import Axios from 'axios';
+import api from './api';
+import { connect } from 'react-redux';
+import tokenAction from './actions/tokenAction';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,6 +18,25 @@ class App extends React.Component {
       loggedIn: false,
       roasts: {},
     };
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Check if token is valid
+      Axios.get(api.verify, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      })
+        .then(res => {
+          if (res.data.loggedIn) {
+            this.setState({ token: token, loggedIn: true, });
+            this.props.tokenAction(this.state.token);
+          }
+        })
+        .catch(err => { });
+    }
   }
 
   render = () => {
@@ -29,4 +52,16 @@ class App extends React.Component {
   }
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    token: state.token,
+  };
+};
+
+const mapDispatchToProps = () => {
+  return {
+    tokenAction,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps())(App);
