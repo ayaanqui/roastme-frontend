3b55f3964eacdc45105bde4db11bfba0d32a7c89
ayaanqui <moahammedayaan.dev@gmail.com>
2020-09-05 10:42:24 -0500

    Added logout endpoint

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ const url = 'http://localhost:3001';
 export default {
   base: url,
   login: `${url}/auth/login`,
+  logout: `${url}/auth/logout`,
   register: `${url}/auth/register`,
   roasts: `${url}/roasts/`,
   verify: `${url}/auth/verify`,
