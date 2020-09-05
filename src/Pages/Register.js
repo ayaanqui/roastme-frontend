ace1194d9f1359323aa4ce817ee06608a69d212d
ayaanqui <moahammedayaan.dev@gmail.com>
2020-09-04 22:18:41 -0500

    Implemented form validation

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -15,38 +15,86 @@ class Register extends React.Component {
       email: '',
       userCreated: false,
       error: false,
+
+      emailIsValid: false,
+      emailIsInvalid: false,
+      usernameIsValid: false,
+      usernameIsInvalid: false,
+      passwordIsValid: false,
+      passwordIsInvalid: false,
     };
   }
 
   handleSubmit = event => {
     this.setState({ loading: true });
 
-    Axios.post(api.register, {
-      email: this.state.email,
-      username: this.state.username,
-      password: this.state.password
-    })
-      .then(res => {
-        this.setState({
-          loading: false,
-          userCreated: true,
-        })
+    if (this.state.emailIsValid && this.state.usernameIsValid && this.state.passwordIsValid) {
+      Axios.post(api.register, {
+        email: this.state.email,
+        username: this.state.username,
+        password: this.state.password
       })
-      .catch(err => {
-        this.setState({
-          loading: false,
-          error: true,
-        })
-      });
-
+        .then(res => this.setState({ userCreated: true, loading: false }))
+        .catch(err => this.setState({ error: true, loading: false }));
+    } else {
+      this.setState({ loading: false });
+    }
     event.preventDefault();
   };
 
-  handleEmailChange = event => this.setState({ email: event.target.value });
-  handleUsernameChange = event => this.setState({ username: event.target.value });
-  handlePasswordChange = event => this.setState({ password: event.target.value });
+  handleEmailChange = event => {
+    const email = event.target.value.toLowerCase().replace(/\s/g, '');
+
+    if (email === '') {
+      this.setState({ emailIsValid: false, emailIsInvalid: false })
+      return null;
+    }
+
+    if (email !== this.state.email) {
+      this.setState({ email: email });
+
+      Axios.post(`${api.base}/auth/check-email`, { email: email })
+        .then(res => {
+          if (res.data.unique)
+            this.setState({ emailIsInvalid: false, emailIsValid: true });
+          else
+            this.setState({ emailIsInvalid: true, emailIsValid: false });
+        })
+        .catch(() => { });
+    }
+  };
+  handleUsernameChange = event => {
+    const username = event.target.value.replace(/\s/g, '');
+
+    if (username === '') {
+      this.setState({ usernameIsValid: false, emailIsInvalid: false })
+      return null;
+    }
+
+    if (username !== this.state.username) {
+      this.setState({ username: username });
+
+      Axios.post(`${api.base}/auth/check-username`, { username: username })
+        .then(res => {
+          if (res.data.unique)
+            this.setState({ usernameIsInvalid: false, usernameIsValid: true });
+          else
+            this.setState({ usernameIsInvalid: true, usernameIsValid: false });
+        })
+        .catch(() => { });
+    }
+  };
+  handlePasswordChange = event => this.setState({ password: event.target.value, passwordIsInvalid: false, passwordIsValid: true });
 
   renderRegisterBtn = () => {
+    if (!this.state.emailIsValid || !this.state.usernameIsValid || !this.state.passwordIsValid) {
+      return (
+        <Button className="mt-3" variant="primary" type="submit" disabled={true}>
+          Register
+        </Button>
+      );
+    }
+
     if (this.state.loading) {
       return (
         <Button className="mt-3" variant="primary" type="submit" disabled={true}>
@@ -91,12 +139,14 @@ class Register extends React.Component {
 
               <Form.Group controlId="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control onChange={this.handleEmailChange} type="text" placeholder="Enter username" />
+                <Form.Control onChange={this.handleEmailChange} type="email" placeholder="Enter email" isValid={this.state.emailIsValid} isInvalid={this.state.emailIsInvalid} />
+                <Form.Control.Feedback type='invalid'>Email is already in use by another user</Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group controlId="usename">
+              <Form.Group controlId="username">
                 <Form.Label>Username</Form.Label>
-                <Form.Control onChange={this.handleUsernameChange} type="text" placeholder="Enter username" />
+                <Form.Control onChange={this.handleUsernameChange} type="text" placeholder="Enter username" isValid={this.state.usernameIsValid} isInvalid={this.state.usernameIsInvalid} />
+                <Form.Control.Feedback type='invalid'>Username has already been taken</Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group controlId="password">
