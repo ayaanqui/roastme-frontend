59d9e5c2cb09f614344c61cd2348a11d0121b907
ayaanqui <moahammedayaan.dev@gmail.com>
2020-09-08 17:43:12 -0500

    Added post route for CreateRoast

--- src/Routes.js
+++ src/Routes.js
@@ -7,10 +7,14 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Explore from './pages/Explore';
+import CreateRoast from './pages/CreateRoast';
 
 const Routes = () => {
   return (
     <Switch>
+      <Route path='/post'>
+        <CreateRoast />
+      </Route>
       <Route path='/login'>
         <Login />
       </Route>
