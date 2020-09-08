2382f3ee8f5ffa5274f944e0b0628abeb8b60a84
ayaanqui <moahammedayaan.dev@gmail.com>
2020-09-08 16:06:37 -0500

    Display roasts

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import Axios from 'axios';
+import api from '../api';
+import { Row, Col, Card, Button } from 'react-bootstrap';
 
-const Home = () => {
-  return (<h1>Home page</h1>);
-};
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      roasts: [],
+      page: 1,
+      limit: 30,
+      loading: false,
+      loaded: false,
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState({ loading: true });
+
+    Axios.get(api.roasts)
+      .then(roasts => this.setState({ roasts: roasts.data, loading: false, loaded: true }))
+      .catch(_ => { });
+  };
+
+  render = () => {
+    return (
+      <>
+        <h1>Homepage</h1>
+        <br />
+        <Row>
+          {this.state.loaded ? this.state.roasts.map(roast => {
+            return (
+              <Col className="mb-4" lg="4">
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src="images/100px180.svg" />
+                  <Card.Body>
+                    <Card.Title>{roast.caption}</Card.Title>
+                    <Card.Text>Hello world this is a text</Card.Text>
+                    <Button variant="primary" size="sm">Expand</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          }) : <h3>Loading</h3>}
+        </Row>
+      </>
+    );
+  }
+}
 
 export default Home;
